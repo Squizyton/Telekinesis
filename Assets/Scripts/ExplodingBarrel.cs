2d9b72929388a5bb98969b6ed1df977c770b495a
Joel <fallendemongod@gmail.com>
2022-05-04 17:39:07 -0400

    Added Exploding barrel prefab

--- Assets/Scripts/ExplodingBarrel.cs
+++ Assets/Scripts/ExplodingBarrel.cs
@@ -9,7 +9,7 @@ public class ExplodingBarrel : Pullable
    [SerializeField]private Vector2 explosionForce;
    [SerializeField]private float explosionRadius = 5f;
    public bool currentlyExploding = false;
-
+   public LayerMask LayerMask;
    public void OnTriggerEnter(Collider other)
    {
        if(thrown)
@@ -17,29 +17,45 @@ public class ExplodingBarrel : Pullable
    }
 
 
-   public void Explode()
+   private void Explode()
    {
        currentlyExploding = true;
-       var objects = Physics.OverlapSphere(transform.position,explosionRadius).ToList();
        
-       foreach(var obj in objects)
+       Debug.Log(gameObject.name + " is exploding");
+       
+       var maxColliders = 50;
+       var hitColliders = new Collider[maxColliders];
+       //No garbage allocation and it's faster
+       var numColliders = Physics.OverlapSphereNonAlloc(transform.position, explosionRadius, hitColliders, LayerMask);
+       Debug.Log(numColliders);
+       
+
+       for (int i = 0; i < numColliders; i++)
        {
            var explosionAmount = UnityEngine.Random.Range(explosionForce.x, explosionForce.y);
-
+           var obj = hitColliders[i].gameObject;
+           Debug.Log(obj.name);
+           
            if (obj.TryGetComponent(out ExplodingBarrel otherBarrel) && !otherBarrel.currentlyExploding)
            {
                otherBarrel.Explode();
            }
-           
-           var rigidbody = obj.GetComponent<Rigidbody>();
-           if(rigidbody != null)
+
+           var rb = obj.GetComponent<Rigidbody>();
+           if (rb != null)
            {
-               rigidbody.AddForce(obj.transform.position - transform.position* explosionAmount  * Time.deltaTime, ForceMode.Impulse);
+               Debug.Log("adding force");
+               rb.AddForce(obj.transform.position - transform.position * explosionAmount * Time.deltaTime,
+                   ForceMode.Impulse);
            }
-           
-           
        }
-       
        Destroy(gameObject);
+   }
+
+
+   private void OnDrawGizmos()
+   {
+       Gizmos.color = Color.red;
+       Gizmos.DrawWireSphere(transform.position, explosionRadius);
    }
 }
